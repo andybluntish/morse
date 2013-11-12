a876c61d196199d9a9f11414dc5747530a16c1d2
Andy Bluntish <andy@bluntish.net>
2013-11-13 00:49:10 +1100

    Refactor into string methods
      * Convert strings, not HTML elements
      * Test encode/decode interface methods

--- README.md
+++ README.md
@@ -4,34 +4,14 @@ Convert text to and from morse code.
 
 ## Usage
 
-Simply call `encode` and pass in a selector to convert the contents of the selected elements to morse code.
+### Encode
 
 ```js
-morse.encode('.morse');
+morse.encode('SOS'); // ... --- ...
 ```
 
-
-```html
-<!-- Before -->
-<p>Here is some text in <span class="morse">morse code</span>.</p>
-
-<!-- After -->
-<p>Here is some text in <span class="morse" data-original-value="morse code">-- --- .-. ... .  -.-. --- -.. .</span>.</p>
-
-```
-
-Decode works just the same.
+### Decode 
 
 ```js
-morse.decode('.morse');
+morse.decode('... --- ...'); // sos
 ```
-
-```html
-<!-- Before -->
-<p>Here is some text in <span class="morse">-- --- .-. ... .  -.-. --- -.. .</span>.</p>
-
-<!-- After -->
-<p>Here is some text in <span class="morse">morse code</span>.</p>
-```
-
-We use `document.querySelectorAll()` internally, so any valid `querySelector` selector will work.
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,31 +1,54 @@
 /*global describe, it */
 'use strict';
+
 (function () {
 
   describe('Morse', function () {
-
-    beforeEach(function() {
-      // setup
-    });
 
     it('should exist', function() {
       expect(morse).to.exist;
     });
 
-    describe('encode', function () {
+    describe('Convert a String', function() {
+
       it('should have an "encode" method', function() {
         expect(morse.encode).to.be.a('function');
       });
 
-      it('should encode an HTML element into Morse code');
-    });
+      describe('encode', function () {
+        before(function() {
+          this.string = 'SOS';
+          this.result = morse.encode(this.string);
+        });
 
-    describe('decode', function () {
-      it('should have an "decode" method', function() {
-        expect(morse.decode).to.be.a('function');
+        it('should return a String', function() {
+          expect(this.result).to.be.a('string');
+        });
+
+        it('should encode a Roman alphabet String into Morse Code', function() {
+          expect(this.result).to.equal('... --- ...');
+        });
       });
 
-      it('should decode an HTML element from Morse code');
+      it('should have an "decode" method', function() {
+        expect(morse.encode).to.be.a('function');
+      });
+
+      describe('decode', function () {
+        before(function() {
+          this.string = '... --- ...';
+          this.result = morse.decode(this.string);
+        });
+
+        it('should return a String', function() {
+          expect(this.result).to.be.a('string');
+        });
+
+        it('should decode a Morse Code String into the Roman alphabet', function() {
+          expect(this.result).to.equal('sos');
+        });
+      });
+
     });
 
   });
